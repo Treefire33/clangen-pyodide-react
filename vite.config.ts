46507b55a887c9810a0ad5f16e03f97a45a70ffc
Braidon <braidonaksingh@gmail.com>
2025-12-13 00:24:44 -0600

    PWA attempt 1
    I have zero clue what I'm doing but I think I have it right

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { dirname, join, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+import { VitePWA } from 'vite-plugin-pwa';
 
 import react from '@vitejs/plugin-react'
 
@@ -33,6 +34,46 @@ export default defineConfig({
   plugins: [
     react(),
     viteStaticCopyPyodide(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ["clangen-simulator-icon.png"],
+      manifest: {
+        name: "ClanGen Simulator",
+        short_name: "ClanGenSim",
+        description: "Play ClanGen online in your web browser with no download! Mobile-friendly.",
+        theme_color: "#655934",
+        background_color: "#4a432b",
+        icons: [
+          {
+            "src": "/public/favicons/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/public/favicons/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/public/favicons/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/public/favicons/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ]
+      }
+    })
   ],
   build: {
     rollupOptions: {
